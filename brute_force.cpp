bcb15fd04c3057f9b89aebca71843d450a4111e9
PAKKAPONG ATTAWIBOON <pakkapong.att@mail.kmutt.ac.th>
2018-12-12 22:29:03 +0700

    Update brute_force.cpp

--- brute_force.cpp
+++ brute_force.cpp
@@ -1,18 +1,15 @@
 #include<stdio.h>
 #include<algorithm>
-using namespace std;
-int type_of_album = 4,album[] = {12,20,32,10};
-int stage[10000],minimum;
+#include<array>
+int type_of_album = 4,album[] = {12,20,32,10},limit_add = 4;
+int stage[10000],minimum,found = 0,ans[10000];
 void brute(int n,int pic,int amount)
 {
     if(n==pic && amount<minimum) //if there are
     {
         minimum=amount;
-        for(int i=0;i<amount;i++)
-        {
-            printf("%d ",stage[i]); //print passed stage
-        }
-        printf("\n");
+        std::copy(std::begin(stage),std::end(stage),std::begin(ans));
+        found=1;
     }
     if(n<pic)
     {
@@ -29,7 +26,24 @@ int compare(int a,int b)
 }
 int main()
 {
+    int n;
     minimum = 99999999;
-    sort(album,album+type_of_album,compare);
-    brute(0,168,0);
+    scanf("%d",&n);
+    std::sort(album,album+type_of_album,compare);
+    brute(0,n,0);
+    if(found==0)
+    {
+        for(int i=1; i<=limit_add; i++)
+        {
+            brute(0,n,0)
+        }
+    }
+    if(found==1)
+    {
+        for(int i=0; i<minimum; i++)
+        {
+            printf("%d ",ans[i]);
+        }
+        printf("\n");
+    }
 }
