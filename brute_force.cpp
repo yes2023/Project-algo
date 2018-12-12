f11045a134e2b8a6812be316a952cd15fb7c2dea
PAKKAPONG ATTAWIBOON <pakkapong.att@mail.kmutt.ac.th>
2018-12-12 23:20:31 +0700

    brute_force complete

--- brute_force.cpp
+++ brute_force.cpp
@@ -1,14 +1,19 @@
 #include<stdio.h>
 #include<algorithm>
-#include<array>
 int type_of_album = 4,album[] = {12,20,32,10},limit_add = 4;
-int stage[10000],minimum,found = 0,ans[10000];
+int stage[10000],minimum,found,ans[10000];
+void copy_arry(int n)
+{
+    for(int i=0;i<n;i++)
+        ans[i]=stage[i];
+}
+
 void brute(int n,int pic,int amount)
 {
     if(n==pic && amount<minimum) //if there are
     {
         minimum=amount;
-        std::copy(std::begin(stage),std::end(stage),std::begin(ans));
+        copy_arry(amount);
         found=1;
     }
     if(n<pic)
@@ -26,17 +31,34 @@ int compare(int a,int b)
 }
 int main()
 {
-    int n;
+    int n,min1;
     minimum = 99999999;
+    found = 0;
     scanf("%d",&n);
     std::sort(album,album+type_of_album,compare);
     brute(0,n,0);
     if(found==0)
     {
-        for(int i=1; i<=limit_add; i++)
+        for(int i=1; i<=limit_add && found==0; i++)
         {
-            brute(0,n,0)
+            brute(0,n+i,0);
+            min1=minimum;
+            brute(0,n-i,0);
+            if(min1==minimum && found)
+            {
+                printf("Add song sai : %d\n",i);
+            }
+            else if(found)
+            {
+                printf("Use %d\n",n+i);
+            }
         }
+        for(int i=limit_add+1;found==0 ; i++)
+            {
+                brute(0,n+i,0);
+                if(found)
+                    printf("Use %d\n",n+i);
+            }
     }
     if(found==1)
     {
