d9069ce8c4b46f5ec89ead6f15d25c3ace51fd96
topty <topty@live.com>
2018-12-17 11:23:46 +0700

    find minimum amount

--- albumdfs.js
+++ albumdfs.js
@@ -2,20 +2,24 @@ const sumAlbum = (albums) => albums.reduce((sum, val) => sum + val, 0)
 
 function dfs(albums, photos) {
 	let stack = [[]]
+	let ans = []
+
 	while(stack.length > 0) {
 		let current = stack.pop()
 		let amount = sumAlbum(current)
-		if(amount == photos) {
-			return current
+
+		if(amount == photos && (ans.length == 0 || current.length < ans.length)) {
+			ans = current
 		}
+
 		albums.forEach(album => {
 			if(amount + album <= photos) {
 				stack.push(current.concat(album))
 			}
 		})
 	} 
-	return []
+	return ans
 }
 
-var ans = dfs([10, 12, 20, 32], 168)
+var ans = dfs([24, 52, 28], 204)
 console.log(ans.reduce((sum, val) => sum + val, 0), ans)
